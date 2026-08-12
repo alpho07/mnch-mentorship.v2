e49567a91cb7ff277bd17ef7e20e36566c69c1a9
Alphonce Ochieng <alpho07@gmail.com>
2026-08-12 06:58:31 +0300

    chore: pint formatting pass for Survey* code (Phase 1 complete)

--- app/Policies/SurveyResponsePolicy.php
+++ app/Policies/SurveyResponsePolicy.php
@@ -2,8 +2,8 @@
 
 namespace App\Policies;
 
-use App\Models\User;
 use App\Models\SurveyResponse;
+use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class SurveyResponsePolicy
